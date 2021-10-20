f0ff4b0fdc40eae09663a20cb383ab9aec1770f6
alessandro faria <cabelo@opensuse.org>
2021-10-20 01:25:06 -0300

    New source

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,2 @@
-g++ -shared pam_realsenseid.cpp -o pam_realsenseid.so -Wl,-rpath,/dados/Fontes/intel/RealSenseID/build/lib  /dados/Fontes/intel/RealSenseID/build/lib/librsid.so  -I/dados/Fontes/intel/RealSenseID/include/  -lpam -fPIC
+dpcpp -shared pam_realsenseid.cpp -o pam_realsenseid.so -Wl,-rpath,/dados/Fontes/intel/RealSenseID/build/lib  /dados/Fontes/intel/RealSenseID/build/lib/librsid.so  -I/dados/Fontes/intel/RealSenseID/include/  -lpam -fPIC
 
